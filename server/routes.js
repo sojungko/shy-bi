8b55eb9af7ad73df130d9183ffd859406e11ff5e
JW Garrison <jwolfgarrison@gmail.com>
2016-12-17 17:15:15 -0500

    feat(#22): Create userController, BE SignUp API route, Create database query to add user

--- server/routes.js
+++ server/routes.js
@@ -1,9 +1,12 @@
 const bodyParser = require('body-parser');
 const path = require('path');
+const userController = require('./user/userController');
 
 module.exports = (app, express) => {
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(bodyParser.json());
   app.use(express.static(path.join(__dirname, '/../node_modules')));
   app.use(express.static(path.join(__dirname, '/../build')));
+
+  app.post('/api/users/signup', userController.users.signup);
 };
