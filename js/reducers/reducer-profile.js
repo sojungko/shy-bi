4e7cb0ea0266bc94b61651b4e30147e558b338cd
Sojung Park <sojungko@gmail.com>
2016-12-19 15:36:34 -0500

    feat(app.js): add reducers, user-list.js

---
+++ js/reducers/reducer-profile.js
@@ -0,0 +1,12 @@
+export default function() {
+  return [
+    {
+      name: 'Justin Garrison',
+      username: 'Jusin',
+      email: 'justin@gmail.com',
+      password: 'justin',
+      city: 'New York, NY',
+      age: 29
+    }
+  ]
+}
