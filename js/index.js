8d92fb1755953c7f9b6aea36d5f40c78e5633904
Sojung Park <sojungko@gmail.com>
2016-12-19 15:38:39 -0500

    fix(routes.js): delete extra routes.js

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { Router, hashHistory } from 'react-router';
 import reducers from './reducers';
-import routes from './routes';
+import routes from './routes/routes';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
