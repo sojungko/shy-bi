7075fa2ecd0ec6786728290c8eded2ec31745f04
Sojung Park <sojungko@gmail.com>
2016-12-20 10:40:08 -0500

    feat(redux): add redux back

--- js/components/login.js
+++ js/components/login.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 
 export default class LogIn extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: '',
+      password: ''
+    }
+  }
+
   render() {
     return (
       <div>
