3a31ebc4b1044a368795c83937570d9657f271cb
Peter Schussheim <peter@schussheim.com>
2016-12-20 11:02:45 -0500

    feat(browser): create actions using axios

---
+++ js/actions/actions.js
@@ -0,0 +1,32 @@
+import * as types from './actionTypes';
+import axios from 'axios';
+import {
+  pushState
+} from 'redux-react-router';
+
+const requestData = () => ({
+  type: types.REQ_DATA
+});
+const receiveData = (json) => ({
+  type: types.RECV_ERROR,
+  data: json
+});
+
+export function fetchData(url) {
+  return function (dispatch) {
+    dispatch(requestData());
+    return axios({
+        url: url,
+        timeout: 20000,
+        method: 'get',
+        responseType: 'json'
+      })
+      .then(function (response) {
+        dispatch(receiveData(response.data));
+      })
+      .catch(function (response) {
+        dispatch(receiveError(response.data));
+        dispatch(pushState(null, '/error'));
+      })
+  }
+}
