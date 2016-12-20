d36a5bbcc99140acc0afaad81685d2a2bf48ec08
Sojung Park <sojungko@gmail.com>
2016-12-20 10:14:16 -0500

    feat(Login.js): add login in routes

--- js/components/login.js
+++ js/components/login.js
@@ -3,9 +3,13 @@ import React, { Component } from 'react';
 export default class LogIn extends Component {
   render() {
     return (
-      <form className="input-group">
-        <input />
-      </form>
+      <div>
+        <h1> Login </h1>
+        <form className="input-group">
+          Username: <input />
+          Password: <input />
+        </form>
+      </div>
     );
   }
 }
