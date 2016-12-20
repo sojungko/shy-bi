eee2b90ab23a0734df83fe1b20587da7010ac57d
Sojung Park <sojungko@gmail.com>
2016-12-20 18:08:52 -0500

    fix(UserList.js): refactor new variable names from database

--- js/reducers/reducer_profile.js
+++ js/reducers/reducer_profile.js
@@ -4,7 +4,7 @@ export default function(state = [], action) {
   console.log('ACTION PAYLOAD IN GET USER : ', action.payload);
   switch(action.type) {
     case GET_USER:
-      return [ action.payload.data, ...state ];
+      return [ action.payload.data[0], ...state ];
   }
   return state;
 }
