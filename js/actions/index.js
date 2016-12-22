5ff6fbaa458b390eb0622ace17b5637824d4b63f
Peter Schussheim <peter@schussheim.com>
2016-12-21 19:29:23 -0500

    style: fix style issues

--- js/actions/index.js
+++ js/actions/index.js
@@ -1,61 +1,48 @@
 import axios from 'axios';
 
-export const GET_ALL_USERS = 'GET_ALL_USERS';
-export const GET_USER = 'GET_USER';
-export const SIGN_UP_USER = 'SIGN_UP_USER';
-export const LOG_IN_USER = 'LOG_IN_USER';
-export const SEARCH_USERS = 'SEARCH_USERS';
+const GET_ALL_USERS = 'GET_ALL_USERS';
+const GET_USER = 'GET_USER';
+const SIGN_UP_USER = 'SIGN_UP_USER';
+const LOG_IN_USER = 'LOG_IN_USER';
 
 export function getAllUsers() {
-  return dispatch => {
-    axios.get('/api/search/all')
-    .then(response => {
+  return function (dispatch) {
+    axios.get('http://localhost:8080/api/search/all')
+    .then((response) => {
       dispatch({
         type: GET_ALL_USERS,
-        payload: response.data
+        payload: response.data,
       });
     })
     .catch((error) => {
-      console.log(error);
-    })
-  }
+      console.error(error);
+    });
+  };
 }
 
-export function getUser(username) {
-  return dispatch => {
-    axios.get(`/api/users/${username}`)
-    .then(response => {
-      dispatch({
-        type: GET_USER,
-        payload: response.data
-      });
-    })
-    .catch((error) => {
-      console.log(error);
-    })
-  }
+export function getUser(user) {
+  const request = axios.get(`http://localhost:8080/api/users/:${user}`); // TODO need to be able to take in username and fetch data based on that
+
+  return {
+    type: GET_USER,
+    payload: request,
+  };
 }
 
 export function signupUser(props) {
-  const request = axios.post('/api/users/signup', props);
+  const request = axios.post('http://localhost:8080/api/users/signup', props);
 
-  return { type: SIGN_UP_USER, payload: request };
+  return {
+    type: SIGN_UP_USER,
+    payload: request,
+  };
 }
 
 export function logIn(props) {
-  axios.post('/api/users/signin', props)
-    .then(response => {
-      console.log('actions/index.js logIn response : ', response)
-      return { type: LOG_IN_USER, payload: response }
-    })
-    .catch(error => {
-      console.log('actions/index.js logIn error : ', error)
-    })
+  const request = axios.post('http://localhost:8080/api/users/signin', props);
 
-}
-
-export function searchUsers(props) {
-  const request = axios.post('/api/search/filter', props);
-
-  return { type: SEARCH_USERS, payload: request };
+  return {
+    type: LOG_IN_USER,
+    payload: request,
+  };
 }
