eebeb616b97380acd0da7f3549daf937c0deed25
Peter Schussheim <peter@schussheim.com>
2016-12-22 10:39:05 -0500

    style: fix code style issues

--- js/reducers/index.js
+++ js/reducers/index.js
@@ -1,15 +1,12 @@
 import { combineReducers } from 'redux';
-import { reducer as FormReducer } from 'redux-form';
+import { reducer as SignupReducer } from 'redux-form';
 import UsersReducer from './reducer_users';
 import ProfileReducer from './reducer_profile';
-import LogInReducer from './reducer_login.jsx';
-
 
 const rootReducer = combineReducers({
   profile: ProfileReducer,
-  form: FormReducer,
+  form: SignupReducer,
   users: UsersReducer,
-  login: LogInReducer,
 });
 
 export default rootReducer;
