0c082b599fbd74eb1f358b66754af9a32dc50886
Peter Schussheim <peter@schussheim.com>
2016-12-21 19:29:23 -0500

    style: fix style issues

--- js/containers/UserList.jsx
+++ js/containers/UserList.jsx
@@ -1,45 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { getAllUsers } from '../actions/index';
-<<<<<<< HEAD
 import { bindActionCreators } from 'redux';
 import SearchBar from './SearchBar';
 // import UserListItem from '../containers/UserListItem.jsx';
 
 class UserList extends Component {
   componentWillMount() {
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-import { bindActionCreators } from 'redux';
-import SearchBar from './SearchBar';
-// import UserListItem from '../containers/UserListItem.jsx';
-
-class UserList extends Component {
-  componentWillMount() {
-=======
-=======
->>>>>>> 696bfd3b1930af53f13b636b40048877bea8281f
-// import { bindActionCreators } from 'redux';
-import UserListItem from '../containers/UserListItem.jsx';
-
-class UserList extends Component {
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
->>>>>>> 696bfd3b1930af53f13b636b40048877bea8281f
-  constructor(props) {
-    super(props);
-  }
-
-<<<<<<< HEAD
-=======
->>>>>>> display-profile-1
->>>>>>> 696bfd3b1930af53f13b636b40048877bea8281f
-  componentWillReceiveProps() {
->>>>>>> style: fix style issues
->>>>>>> refactor: style
     this.props.getAllUsers();
   }
 
@@ -54,17 +21,7 @@ class UserList extends Component {
   render() {
     return (
       <div>
-<<<<<<< HEAD
         <SearchBar></SearchBar>
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-        <SearchBar></SearchBar>
-=======
->>>>>>> style: fix style issues
-=======
->>>>>>> 696bfd3b1930af53f13b636b40048877bea8281f
->>>>>>> refactor: style
         <ul className="list-group col-sm-4">
           {this.renderList()}
         </ul>
