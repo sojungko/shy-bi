48d0936dda17e217aea1a10123bb47d0c4106083
JW Garrison <jwolfgarrison@gmail.com>
2017-01-02 17:28:34 -0500

    refactor(FE):

---
+++ src/index.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Router, browserHistory } from 'react-router';
+import ReduxThunk from 'redux-thunk';
+
+import routes from './routes/Routes.jsx';
+import reducers from './reducers';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+console.log('INDEX.JSX | Importing REDUCERS');
+console.log('INDEX.JSX | Importing ROUTES');
+
+console.log('INDEX.JSX | Creating Redux Store and Applying Middlewears: Redux-Promise');
+
+const createStoreWithMiddleware = composeEnhancers(applyMiddleware(ReduxThunk))(createStore);
+
+console.log('INDEX.JSX | Preparing to Render APP Component via React Router');
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <Router history={browserHistory} routes={routes}>
+    </Router>
+  </Provider>,
+  document.getElementById('root')
+);
