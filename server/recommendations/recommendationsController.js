ce711bdecb5b6b51974494549ed52c0ce8c43ab0
Peter Schussheim <peter@schussheim.com>
2017-01-02 15:48:05 -0500

    feat(server): start building out recommendationsModel and controller

---
+++ server/recommendations/recommendationsController.js
@@ -0,0 +1,6 @@
+
+module.exports = {
+  getRecommendedMatches({ query }, res) {
+    console.log(`1) [recommendationsController.js/getRecommendedMatches] Getting recommended matches for ${query.user}`);
+  }
+}
