cfdd231829164d32460c67423aa4818ed08685f4
JW Garrison <jwolfgarrison@gmail.com>
2017-01-02 22:46:00 -0500

    fix(Actions): Prefer Import/Export statements, Use HashHistory

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,28 +1,31 @@
 import axios from 'axios';
-import { browserHistory } from 'react-router';
-const jwt = require('jwt-simple');
+import { hashHistory } from 'react-router';
+import jwt from 'jwt-simple';
 
-module.exports = {
-  /* -- Fetching Users --*/
-  getAllUsers() {
-    return (dispatch) => {
-      axios.get('/api/search/all')
-      .then(response => {
-        dispatch({
-          type: 'GET_ALL_USERS',
-          payload: response.data,
-        });
-      })
-      .catch((error) => {
-        console.error(error);
+console.log('ACTIONS | Exporting ACTIONS...');
+
+export const GET_ALL_USERS = 'GET_ALL_USERS';
+
+/* -- Fetching Users --*/
+export function getAllUsers() {
+  return (dispatch) => {
+    axios.get('/api/search/all')
+    .then((response) => {
+      dispatch({
+        type: GET_ALL_USERS,
+        payload: response.data,
       });
-    };
-  },
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+  };
+}
 
-  getUser(username) {
-    return (dispatch) => {
-      axios.get(`/api/users/${username}`)
-      .then(response => {
+export function getUser(username) {
+  return (dispatch) => {
+    axios.get(`/api/users/${username}`)
+      .then((response) => {
         console.log('actions/index getUser response.data : ', response.data);
         dispatch({
           type: 'GET_USER',
@@ -32,56 +35,58 @@ module.exports = {
       .catch((error) => {
         console.log(error);
       });
-    };
-  },
+  };
+}
 
-  searchUsers(props) {
-    const request = axios.post('/api/search/filter', props);
-    return { type: 'SEARCH_USERS', payload: request };
-  },
+export function searchUsers(props) {
+  const request = axios.post('/api/search/filter', props);
+  return { type: 'SEARCH_USERS', payload: request };
+}
 
-  /* -- Signing up User--*/
-  signupUser(props) {
-    return (dispatch) => {
-      axios.post('/auth/signup', props)
-      .then(response => {
+/* -- Signing up User--*/
+export function signupUser(props) {
+  return (dispatch) => {
+    axios.post('/auth/signup', props)
+      .then((response) => {
         dispatch({ type: 'SIGN_UP_USER', payload: response.data });
-        browserHistory.push('/search');
+        hashHistory.push('/search');
       })
-      .catch(error => {
+      .catch((error) => {
         console.log('actions/index signupUser error : ', error);
       });
-    };
-  },
+  };
+}
 
-  /* -- Logging in User--*/
-  loginUserFailure(error) {
-    return {
-      type: 'LOGIN_USER_FAILURE',
-      payload: error,
-    }
-  },
+/* -- Logging in User--*/
+export function loginUserFailure(error) {
+  return {
+    type: 'LOGIN_USER_FAILURE',
+    payload: error,
+  };
+}
 
-  loginUser(props) {
-    // V.1
-    // const request = axios.post('/api/users/signin', props);
-    // return { type: 'LOGIN_USER_SUCCESS', payload: request };
+export function loginUser(props) {
+  // V.1
+  // const request = axios.post('/api/users/signin', props);
+  // return { type: 'LOGIN_USER_SUCCESS', payload: request };
 
-    // V.2
-    return (dispatch) => {
-      console.log('actions/index loginUser props : ', props);
-      axios.post('/auth/signin', props)
+  // V.2
+  return (dispatch) => {
+    console.log('actions/index loginUser props : ', props);
+    axios.post('/auth/signin', props)
       .then((response) => {
         console.log('actions/index loginUser resopnse.data : ', response.data);
         localStorage.setItem('token', response.data.token);
         dispatch({ type: 'LOGIN_USER_SUCCESS', payload: response.data });
       })
       .then(() => {
-        browserHistory.push('/profile');
+        hashHistory.push('/profile');
       })
       .catch((error) => {
         console.log('actions/index loginUser error : ', error);
       });
-    };
-  },
+  };
 }
+
+console.log('ACTIONS | Exported ACTIONS');
+console.log('');
