d3018dc5f7612017640af337fa79838cca866cbc
JW Garrison <jwolfgarrison@gmail.com>
2017-01-02 22:33:53 -0500

    refactor(Nav Bar): Prefer stateless functional components

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,30 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router';
 
 console.log('COMPONENT/NAV BAR | Exporting NAV BAR...');
 
-export default class Navbar extends Component {
-  componentDidMount() {
-    console.log('    COMPONENT/NAV BAR | Complete Rendering NAV BAR');
-  }
+const Navbar = () => {
+  console.log('    COMPONENT/NAV BAR | Rendering NAV BAR Component...');
+  return (
+    <ul>
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/search">Search</Link></li>
+      <li><Link to="/profile">Profile</Link></li>
+      <li><Link to="/signup">Signup</Link></li>
+      <li><Link to="/login">Login</Link></li>
+    </ul>
+  );
+};
 
-  componentDidUpdate() {
-    console.log('    COMPONENT/NAV BAR | Complete Rendering NAV BAR');
-  }
-
-  render() {
-    console.log('    COMPONENT/NAV BAR | Rendering NAV BAR Component...');
-    return (
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/search">Search</Link></li>
-        <li><Link to="/profile">Profile</Link></li>
-        <li><Link to="/signup">Signup</Link></li>
-        <li><Link to="/login">Login</Link></li>
-      </ul>
-    );
-  }
-}
-
+export default Navbar;
 console.log('COMPONENT/NAV BAR | Exported APP');
 console.log(' ');
