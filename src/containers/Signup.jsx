a73d7a95301306271c9fab6dee87a27b75862d13
Sojung Park <sojungko@gmail.com>
2017-01-03 14:52:48 -0500

    feat(Navbar, App): Fix MuiThemeProvider issues, add toggle drawer, tabs

--- src/containers/Signup.jsx
+++ src/containers/Signup.jsx
@@ -1,7 +1,6 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
-import { Link, hashHistory } from 'react-router';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import { Link } from 'react-router';
 import RaisedButton from 'material-ui/RaisedButton';
 import TextField from 'material-ui/TextField';
 import { Card, CardText } from 'material-ui/Card';
@@ -118,103 +117,101 @@ class SignUp extends Component {
     console.log('    CONTAINER/SIGN UP | Rendering SIGN UP Container... ');
 
     return (
-      <MuiThemeProvider>
-        <div>
-          <Card className="container">
-            <form onSubmit={this.onSubmit}>
-              <h2 className="card-heading">Sign Up</h2>
+      <div>
+        <Card className="container">
+          <form onSubmit={this.onSubmit}>
+            <h2 className="card-heading">Sign Up</h2>
 
-              {this.state.errors.summary && <p className="error-message">{this.state.errors.summary}</p>}
+            {this.state.errors.summary && <p className="error-message">{this.state.errors.summary}</p>}
 
-              <div className="field-line">
-                <TextField
-                  floatingLabelText="Username"
-                  name="username"
-                  type="text"
-                  errorText={this.state.errors.username}
-                  onChange={this.onUsernameChange}
-                  value={this.state.username}
+            <div className="field-line">
+              <TextField
+                floatingLabelText="Username"
+                name="username"
+                type="text"
+                errorText={this.state.errors.username}
+                onChange={this.onUsernameChange}
+                value={this.state.username}
+              />
+            </div>
+            <div className="field-line">
+              <TextField
+                floatingLabelText="Password"
+                type="password"
+                name="password"
+                onChange={this.onPasswordChange}
+                errorText={this.state.errors.password}
+                value={this.state.password}
+              />
+            </div>
+            <div className="field-line">
+              <TextField
+                floatingLabelText="Name"
+                name="name"
+                type="text"
+                errorText={this.state.errors.name}
+                onChange={this.onNameChange}
+                value={this.state.name}
+              />
+            </div>
+            <div className="field-line">
+              <TextField
+                floatingLabelText="Email"
+                type="email"
+                name="email"
+                errorText={this.state.errors.email}
+                onChange={this.onEmailChange}
+                value={this.state.email}
+              />
+            </div>
+            <div className="field-line">
+              <TextField
+                floatingLabelText="Age"
+                type="number"
+                name="age"
+                errorText={this.state.errors.age}
+                onChange={this.onAgeChange}
+                value={this.state.age}
+              />
+            </div>
+            <div className="field-line">
+              <RadioButtonGroup
+                name="sex"
+                defaultSelected="male"
+                errorText={this.state.errors.sex}
+                onChange={this.onSexChange}
+                valueSelected={this.state.sex}
+              >
+                <RadioButton
+                  value="male"
+                  label="Male"
+                  style={styles.radioButton}
                 />
-              </div>
-              <div className="field-line">
-                <TextField
-                  floatingLabelText="Password"
-                  type="password"
-                  name="password"
-                  onChange={this.onPasswordChange}
-                  errorText={this.state.errors.password}
-                  value={this.state.password}
+                <RadioButton
+                  value="female"
+                  label="Female"
+                  style={styles.radioButton}
                 />
-              </div>
-              <div className="field-line">
-                <TextField
-                  floatingLabelText="Name"
-                  name="name"
-                  type="text"
-                  errorText={this.state.errors.name}
-                  onChange={this.onNameChange}
-                  value={this.state.name}
-                />
-              </div>
-              <div className="field-line">
-                <TextField
-                  floatingLabelText="Email"
-                  type="email"
-                  name="email"
-                  errorText={this.state.errors.email}
-                  onChange={this.onEmailChange}
-                  value={this.state.email}
-                />
-              </div>
-              <div className="field-line">
-                <TextField
-                  floatingLabelText="Age"
-                  type="number"
-                  name="age"
-                  errorText={this.state.errors.age}
-                  onChange={this.onAgeChange}
-                  value={this.state.age}
-                />
-              </div>
-              <div className="field-line">
-                <RadioButtonGroup
-                  name="sex"
-                  defaultSelected="male"
-                  errorText={this.state.errors.sex}
-                  onChange={this.onSexChange}
-                  valueSelected={this.state.sex}
-                >
-                  <RadioButton
-                    value="male"
-                    label="Male"
-                    style={styles.radioButton}
-                  />
-                  <RadioButton
-                    value="female"
-                    label="Female"
-                    style={styles.radioButton}
-                  />
-                </RadioButtonGroup>
-              </div>
-              <div className="field-line">
-                <TextField
-                  floatingLabelText="City"
-                  type="text"
-                  name="city"
-                  errorText={this.state.errors.city}
-                  onChange={this.onCityChange}
-                  value={this.state.city}
-                />
-              </div>
-              <div className="button-line">
-                <RaisedButton type="submit" label="Create New Account" primary />
-              </div>
+              </RadioButtonGroup>
+            </div>
+            <div className="field-line">
+              <TextField
+                floatingLabelText="City"
+                type="text"
+                name="city"
+                errorText={this.state.errors.city}
+                onChange={this.onCityChange}
+                value={this.state.city}
+              />
+            </div>
+            <div className="button-line">
+              <RaisedButton type="submit" label="Create New Account" primary />
+            </div>
 
-              <CardText>Already have an account? <Link to={'/login'}>Log in</Link></CardText>
-            </form>
-          </Card>
-        </div>
-      </MuiThemeProvider>
+            <CardText>Already have an account? <Link to={'/login'}>Log in</Link></CardText>
+          </form>
+        </Card>
+      </div>
     );
   }
 
--- src/index.jsx
+++ src/index.jsx
@@ -4,12 +4,14 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import { Router, hashHistory } from 'react-router';
 import ReduxThunk from 'redux-thunk';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 
-import routes from './routes/Routes.jsx';
+import routes from './routes/Routes';
 import reducers from './reducers';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+injectTapEventPlugin();
 
 console.log('INDEX.JSX | Importing REDUCERS');
 console.log('INDEX.JSX | Importing ROUTES');
@@ -20,8 +22,10 @@ const createStoreWithMiddleware = composeEnhancers(applyMiddleware(ReduxThunk))(
 
 console.log('INDEX.JSX | Preparing to Render APP Component via React Router');
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <Router history={hashHistory} routes={routes} />
-  </Provider>,
+  <MuiThemeProvider>
+    <Provider store={createStoreWithMiddleware(reducers)}>
+      <Router history={hashHistory} routes={routes} />
+    </Provider>
+  </MuiThemeProvider>,
   document.getElementById('root'),
 );
