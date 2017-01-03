12bfc300f49be5a37b79f7e79d9b2810d781564f
Sojung Park <sojungko@gmail.com>
2017-01-03 09:57:16 -0500

    fix(reducer/users): replace state each time user list refreshes

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -2,8 +2,7 @@ export default function (state = [], action) {
   switch (action.type) {
     case 'GET_ALL_USERS':
       console.log('reducers/users action.payload : ', action.payload);
-      console.log('reducers/users state : ', { ...state, ...action.payload });
-      return [ ...state, ...action.payload ];
+      return action.payload;
     case 'SIGN_UP_USER':
       console.log('reducers/users.js SIGN_UP_USER action.payload : ', action.payload);
       return action.payload;
