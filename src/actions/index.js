1ccf7a87b4e628c28f5f719646f9e777d84e1518
JW Garrison <jwolfgarrison@gmail.com>
2017-01-02 23:37:22 -0500

    fix(Actions): Document, Log In and Sign Up redirects to Profile

--- src/actions/index.js
+++ src/actions/index.js
@@ -13,13 +13,15 @@ export const LOGIN_USER_SUCCESS = 'LOGIN_USER_SUCCESS';
 
 /* -- Fetching Users --*/
 export function getAllUsers() {
+  console.log('      ACTIONS/GET_ALL_USERS | Fetching All Users');
+
   return (dispatch) => {
+    console.log('      ACTIONS/GET_ALL_USERS | Making GET Request to BE: /api/search/all');
+
     axios.get('/api/search/all')
-    .then((response) => {
-      dispatch({
-        type: GET_ALL_USERS,
-        payload: response.data,
-      });
+    .then(({ data }) => {
+      console.log('      ACTIONS/GET_ALL_USERS | Recevied Data from BE: ', data);
+      dispatch({ type: GET_ALL_USERS, payload: data });
     })
     .catch((error) => {
       console.error(error);
@@ -28,14 +30,15 @@ export function getAllUsers() {
 }
 
 export function getUser(username) {
+  console.log(`      ACTIONS/GET_USER | Fetching User: ${username}`);
+
   return (dispatch) => {
+    console.log(`      ACTIONS/GET_USER | Making GET Request to BE: /api/users/${username}`);
+
     axios.get(`/api/users/${username}`)
-      .then((response) => {
-        console.log('actions/index getUser response.data : ', response.data);
-        dispatch({
-          type: GET_USER,
-          payload: response.data,
-        });
+      .then(({ data }) => {
+        console.log(`      ACTIONS/GET_USER | Recevied Data from BE: ${data}`);
+        dispatch({ type: GET_USER, payload: data });
       })
       .catch((error) => {
         console.log(error);
@@ -44,17 +47,27 @@ export function getUser(username) {
 }
 
 export function searchUsers(props) {
+  console.log('      ACTIONS/SEARCH_USERS | Searching Users', props);
   const request = axios.post('/api/search/filter', props);
   return { type: SEARCH_USERS, payload: request };
 }
 
 /* -- Signing up User--*/
 export function signupUser(props) {
+  console.log('      ACTIONS/SIGN_UP_USER | Signing Up: ', props);
+
   return (dispatch) => {
+    console.log('      ACTIONS/SIGN_UP_USER | Making POST Request to BE: /auth/signup');
+
     axios.post('/auth/signup', props)
-      .then((response) => {
-        dispatch({ type: SIGN_UP_USER, payload: response.data });
-        hashHistory.push('/search');
+      .then(({ data }) => {
+        console.log('      ACTIONS/SIGN_UP_USER | Recevied Data from BE: ', data);
+
+        dispatch({ type: SIGN_UP_USER, payload: data });
+      })
+      .then(() => {
+        console.log('      ACTIONS/SIGN_UP_USER | Success, Redirecting User to /profile');
+        hashHistory.push('/profile');
       })
       .catch((error) => {
         console.log('actions/index signupUser error : ', error);
@@ -64,6 +77,7 @@ export function signupUser(props) {
 
 /* -- Logging in User--*/
 export function loginUserFailure(error) {
+  console.log('      ACTIONS/LOGIN_USER_FAILURE | Could not log in : ', error);
   return {
     type: LOGIN_USER_FAILURE,
     payload: error,
@@ -76,19 +90,24 @@ export function loginUser(props) {
   // return { type: 'LOGIN_USER_SUCCESS', payload: request };
 
   // V.2
+  console.log('      ACTIONS/LOGIN_USER_SUCCESS | Logging in : ', props);
+
   return (dispatch) => {
-    console.log('actions/index loginUser props : ', props);
+    console.log('      ACTIONS/LOGIN_USER_SUCCESS | Making POST Request to BE: /auth/signin');
+
     axios.post('/auth/signin', props)
-      .then((response) => {
-        console.log('actions/index loginUser resopnse.data : ', response.data);
-        localStorage.setItem('token', response.data.token);
-        dispatch({ type: LOGIN_USER_SUCCESS, payload: response.data });
+      .then(({ data }) => {
+        console.log('      ACTIONS/LOGIN_USER_SUCCESS | Recevied Data from BE: ', data);
+        console.log('      ACTIONS/LOGIN_USER_SUCCESS | Setting token to loca storage ', data.token);
+        localStorage.setItem('token', data.token);
+        dispatch({ type: LOGIN_USER_SUCCESS, payload: data });
       })
       .then(() => {
+        console.log('      ACTIONS/LOGIN_USER_SUCCESS | Success, Redirecting User to /profile');
         hashHistory.push('/profile');
       })
       .catch((error) => {
-        console.log('actions/index loginUser error : ', error);
+        console.log('      ACTIONS/LOGIN_USER_SUCCESS | ', error);
       });
   };
 }
