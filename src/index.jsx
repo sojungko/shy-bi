3c34ec134b5e5db6d2478eb5fd670076510d04c3
JW Garrison <jwolfgarrison@gmail.com>
2017-01-04 10:20:52 -0500

    fix(Material-UI): Inject injectTapEventPlugin back to App

--- src/index.jsx
+++ src/index.jsx
@@ -5,13 +5,11 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import { Router, hashHistory } from 'react-router';
 import thunk from 'redux-thunk';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import injectTapEventPlugin from 'react-tap-event-plugin';
 
 import routes from './routes/Routes';
 import RootReducer from './reducers/index';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-injectTapEventPlugin();
 
 console.log('INDEX.JSX | Importing REDUCERS');
 console.log('INDEX.JSX | Importing ROUTES');
@@ -19,12 +17,17 @@ console.log('INDEX.JSX | Importing ROUTES');
 console.log('INDEX.JSX | Creating Redux Store and Applying Middlewears: Redux Thunk');
 console.log(' ');
 
-const store = composeEnhancers(applyMiddleware(thunk))(createStore);
+const store = createStore(
+  RootReducer,
+  composeEnhancers(
+    applyMiddleware(thunk),
+  ),
+);
 
 console.log('INDEX.JSX | Preparing to Render APP Component via React Router');
 ReactDOM.render(
   <MuiThemeProvider>
-    <Provider store={store(RootReducer)}>
+    <Provider store={store}>
       <Router history={hashHistory} routes={routes} />
     </Provider>
   </MuiThemeProvider>,
