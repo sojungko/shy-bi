8fc266cdd33589566d0b357a02165ebc62f64822
Sojung Park <sojungko@gmail.com>
2017-01-05 15:24:58 -0500

    fix(place presentational components back into classes):
    g

--- src/containers/Signup.jsx
+++ src/containers/Signup.jsx
@@ -63,6 +63,23 @@ class SignUp extends Component {
         this.context.router.push('/');
       });
   }
+  renderTextField = ({ input, label, meta: { touched, error }, ...custom }) => (
+     <TextField
+       hintText={label}
+       floatingLabelText={label}
+       errorText={touched && error}
+       {...input}
+       {...custom}
+     />
+   )
+   -
+   renderRadioGroup = ({ input, ...rest }) => (
+     <RadioButtonGroup
+       {...input} {...rest}
+       valueSelected={input.value}
+       onChange={(event, value) => input.onChange(value)}
+     />
+   )
 
   render() {
     console.log('    CONTAINER/SIGN UP | Rendering SIGN UP Container... ');
