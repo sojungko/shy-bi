fa368bacb882a656cb989eb16bf67de6b72c2f0d
Sojung Park <sojungko@gmail.com>
2017-01-05 13:43:16 -0500

    refactor(SearchBar): try to refactor redux-form into SearchBar

--- src/components/Presentational.jsx
+++ src/components/Presentational.jsx
@@ -2,21 +2,25 @@ import React, { PropTypes } from 'react';
 import { RadioButtonGroup } from 'material-ui/RadioButton';
 import TextField from 'material-ui/TextField';
 
-export const renderTextField = ({ input, label, meta: { touched, error }, ...custom }) => (
-  <TextField
-    hintText={label}
-    floatingLabelText={label}
-    errorText={touched && error}
-    {...input}
-    {...custom}
-  />
-);
+export const renderTextField = ({ input, label, meta: { touched, error }, listener,...custom }) => {
+  console.log('PRESENTATIONAL | renderTextField input.value : ', input.value);
+  return (
+    <input
+      {...input}
+      placeholder={label}
+    />
+  );
+};
 
-export const renderRadioGroup = ({ input, ...rest }) => (
-  <RadioButtonGroup
-    valueSelected={input.value}
-    onChange={(event, value) => input.onChange(value)}
-    {...input}
-    {...rest}
-  />
-);
+
+export const renderRadioGroup = ({ input, ...rest }) => {
+  console.log('PRESENTATIONAL | renderRadioGroup input : ', input);
+  return (
+    <RadioButtonGroup
+      valueSelected={input.value}
+      onChange={(event, value) => input.onChange(value)}
+      {...input}
+      {...rest}
+    />
+  );
+}
