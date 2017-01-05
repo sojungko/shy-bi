cbfba5ebbf9f5cdb136bdad4e9d592a6f76b2515
Sojung Park <sojungko@gmail.com>
2017-01-05 17:54:30 -0500

    feat(bioModel, ImageUpload): image upload successfully saves data to database

--- server/bio/bioController.js
+++ server/bio/bioController.js
@@ -1,9 +1,10 @@
 const { postImage } = require('./bioModel');
 
 module.exports = {
-  uploadImage({ body }, res) {
-    console.log(`1) [bioController.js/uploadImage] ${body.senderID} is sending image url ${body.secure_url}`);
-    postImage(body, () => {
+  uploadImage({ body: { username, url } }, res) {
+    console.log(`1) [bioController.js/uploadImage] Sending image url ${url} for ${username}`);
+    postImage(username, url, (records) => {
+      console.log('Image successfully saved : ', records);
       res.status(201).end();
     });
   },
