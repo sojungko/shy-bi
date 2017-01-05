0a739c145df4b83ae7042191a8049a234f201d96
JW Garrison <jwolfgarrison@gmail.com>
2017-01-05 15:48:09 -0500

    feat(Render all messages received by user):

--- src/reducers/messagesReducer.js
+++ src/reducers/messagesReducer.js
@@ -1,9 +1,9 @@
 import { GET_ALL_MESSAGES } from '../actions';
 
-export default function (state = {}, action) {
+export default function (state = [], action) {
   switch (action.type) {
     case GET_ALL_MESSAGES:
-      return { ...state, ...action.payload };
+      return [...state, ...action.payload];
     default:
       return state;
   }
