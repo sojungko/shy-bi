f1beb0f86f8fe24dbb96426a0acecd6aaf581af7
Peter Schussheim <peter@schussheim.com>
2017-01-06 16:43:22 -0500

    feat(browser): update usersReducer to handle action type LIKED_USERS

--- src/reducers/usersReducer.js
+++ src/reducers/usersReducer.js
@@ -1,4 +1,4 @@
-import { GET_ALL_USERS, GET_RECOMMENDED_USERS, SIGN_UP_USER } from '../constants/ActionTypes';
+import { GET_ALL_USERS, GET_RECOMMENDED_USERS, SIGN_UP_USER, LIKED_USERS } from '../constants/ActionTypes';
 
 export default function (state = [], action) {
   switch (action.type) {
@@ -7,6 +7,8 @@ export default function (state = [], action) {
     case SIGN_UP_USER:
       return action.payload;
     case GET_RECOMMENDED_USERS:
+      return action.payload;
+    case LIKED_USERS:
       return action.payload;
     default:
       return state;
