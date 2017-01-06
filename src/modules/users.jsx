eca5f1069890ee1c53d7352628f7ae5638b1e5eb
Peter Schussheim <peter@schussheim.com>
2017-01-06 16:02:14 -0500

    feat(start building liked users list):

--- src/modules/users.jsx
+++
@@ -1,9 +0,0 @@
-import react from 'react';
-import Likes from '../containers/Likes';
-import UserListI
-
-export default function renderUsers(users) {
-  return users.map((user, idx) => 
-    <User key={idx} user={user} />,
-  );
-}
