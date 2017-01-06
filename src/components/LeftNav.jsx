ace6f31a2ad30d7ca777bb1c8069c2169eb79e32
JW Garrison <jwolfgarrison@gmail.com>
2017-01-05 20:10:07 -0500

    feat(Nav Bar): Add divider

--- src/components/LeftNav.jsx
+++ src/components/LeftNav.jsx
@@ -2,6 +2,7 @@ import React, { PropTypes } from 'react';
 import Drawer from 'material-ui/Drawer';
 import Menu from 'material-ui/Menu';
 import MenuItem from 'material-ui/MenuItem';
+import Divider from 'material-ui/Divider';
 import { Link } from 'react-router';
 
 const LeftNav = (props) => {
@@ -10,6 +11,7 @@ const LeftNav = (props) => {
       <Drawer open={props.open}>
         <Menu onItemTouchTap={props.handleToggle}>
           <MenuItem containerElement={<Link to="/" />}>Home</MenuItem>
+          <Divider />
           <MenuItem containerElement={<Link to="/login" />}>Log In</MenuItem>
           <MenuItem containerElement={<Link to="/signup" />}>Sign Up</MenuItem>
           <MenuItem onTouchTap={props.handleToggle}>Close</MenuItem>
@@ -22,11 +24,13 @@ const LeftNav = (props) => {
     <Drawer open={props.open}>
       <Menu onItemTouchTap={props.handleToggle}>
         <MenuItem containerElement={<Link to="/" />}>Home</MenuItem>
+        <Divider />
         <MenuItem containerElement={<Link to="/search" />}>Search</MenuItem>
         <MenuItem containerElement={<Link to={`/likes/${props.user}`} />}>Likes</MenuItem>
         <MenuItem containerElement={<Link to="messages" />}>Messages</MenuItem>
         <MenuItem containerElement={<Link to="/recommended" />}>Recommended</MenuItem>
         <MenuItem containerElement={<Link to="/myaccount" />}>My Account</MenuItem>
+        <Divider />
         <MenuItem onTouchTap={props.handleToggle}>Close</MenuItem>
       </Menu>
     </Drawer>
