9a8e50c969b9af0fc99fc9746490231db6ffc047
JW Garrison <jwolfgarrison@gmail.com>
2017-01-07 13:03:42 -0500

    feat(Can filter by age range, city):

--- src/actions/FilterInputActions.js
+++ src/actions/FilterInputActions.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import * as A from '../constants/ActionTypes';
 
 const updateMinAge = input => ({ type: A.UPDATE_MINAGE, payload: input });
@@ -5,4 +6,34 @@ const updateMaxAge = input => ({ type: A.UPDATE_MAXAGE, payload: input });
 const updateCity = input => ({ type: A.UPDATE_CITY, payload: input });
 const clearFields = () => ({ type: A.CLEAR_FIELDS });
 
-export { updateMinAge, updateMaxAge, updateCity, clearFields };
+// export function getAllUsers() {
+//   return dispatch => axios.get('/api/search/all')
+//     .then(({ data }) => dispatch({ type: A.GET_ALL_USERS, payload: data }))
+//     .catch((error) => {
+//       console.error(error);
+//     });
+// }
+
+const filterUser = ({ minage, maxage, sex, city }) => {
+  let url = '/api/search/filter?';
+
+  !minage ? minage = 19 : minage < 19 ? minage = '19' : minage;
+  !maxage ? maxage = '100' : maxage < minage ? maxage = minage : maxage;
+  url += `minage=${minage}&maxage=${maxage}&`;
+
+  if (sex) {
+    url += `sex=${sex}&`;
+  }
+
+  if (city) {
+    url += `city=${city}`;
+  }
+
+  return dispatch => axios.get(url)
+    .then(({ data }) => dispatch({ type: A.GET_ALL_USERS, payload: data }))
+    .catch((error) => {
+      console.error(error);
+    });
+};
+
+export { updateMinAge, updateMaxAge, updateCity, clearFields, filterUser };
