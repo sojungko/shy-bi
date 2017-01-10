24ac8f480b97044b5617de2b2f13d2f8265021e9
JW Garrison <jwolfgarrison@gmail.com>
2017-01-10 11:41:44 -0500

    refactor(UserList): Seperate presentational logic into UserListItem component
    Fix an issue where UserList was handling the presentational logic. All the presentational HTML mark
    
    ups have been delegated to UserListItems component

--- src/components/UserListItem.jsx
+++ src/components/UserListItem.jsx
@@ -1,34 +1,33 @@
 import React, { PropTypes } from 'react';
+import { getUsername } from '../modules/auth';
 
-const UserListItem = ({ user, handleClick }) => {
-  const { name, sex, age, city, image_url, username } = user;
+const UserListItem = ({ users, handleClick }) => {
+  const renderUserList = () => users
+    .filter(user => user.username !== getUsername())
+    .map((user, index) => (
+      <li key={index} onClick={() => handleClick(user.username)}>
+        <img role="presentation" src={user.image_url} />
+        <h3>{user.name}</h3>
+        <h5>Sex: {user.sex}</h5>
+        <h5>Age: {user.age}</h5>
+        <h5>City: {user.city}</h5>
+      </li>),
+    );
 
-  const onClick = () => {
-    handleClick(username);
-  };
-
-  return (
-    <div onClick={onClick}>
-      <img role="presentation" src={image_url} />
-      <li>
-        <h3>{name}</h3>
-        <h5>Sex: {sex}</h5>
-        <h5>Age: {age}</h5>
-        <h5>City: {city}</h5>
-      </li>
-    </div>
-  );
+  return <ul>{renderUserList()}</ul>;
 };
 
 UserListItem.propTypes = {
-  user: PropTypes.shape({
-    name: PropTypes.string,
-    sex: PropTypes.string,
-    age: PropTypes.string,
-    city: PropTypes.string,
-    username: PropTypes.string,
-    image_url: PropTypes.string,
-  }),
+  users: PropTypes.arrayOf(PropTypes.shape({
+    user: PropTypes.shape({
+      name: PropTypes.string,
+      sex: PropTypes.string,
+      age: PropTypes.string,
+      city: PropTypes.string,
+      username: PropTypes.string,
+      image_url: PropTypes.string,
+    }),
+  })),
   handleClick: PropTypes.func.isRequired,
 };
 
