26121c792e586609acd46b986f693b6d257752e0
JW Garrison <jwolfgarrison@gmail.com>
2017-01-10 11:06:41 -0500

    fix(Search): Show only other users in ther search result
    Fix an issue where user can see themselves in the search result page

--- src/modules/auth.js
+++ src/modules/auth.js
@@ -1,18 +1,8 @@
-/**
- * Authenticate a user. Save a token string in Local Storage
- *
- * @param {string} token
- */
 export function authenticateUser(token, username) {
   localStorage.setItem('token', token);
   localStorage.setItem('username', username);
 }
 
-/**
- * Check if a user is authenticated - check if a token is saved in Local Storage
- *
- * @returns {boolean}
- */
 export function isUserAuthenticated() {
   return localStorage.getItem('token') !== null;
 }
@@ -21,20 +11,10 @@ export function getUsername() {
   return localStorage.getItem('username');
 }
 
-/**
- * Deauthenticate a user. Remove a token from Local Storage.
- *
- */
 export function deauthenticateUser() {
   localStorage.removeItem('token');
   localStorage.removeItem('username');
 }
-
-/**
- * Get a token value.
- *
- * @returns {string}
- */
 
 export function getToken() {
   return localStorage.getItem('token');
