eb4f3c8792f548f37382a5f1ded63b3ab6160ded
JW Garrison <jwolfgarrison@gmail.com>
2017-01-10 18:28:07 -0500

    fix(Search): All user related search returns imgurl, online status

--- server/recommendations/recommendationsController.js
+++ server/recommendations/recommendationsController.js
@@ -5,20 +5,20 @@ module.exports = {
   getRecommendedMatches({ params }, res) {
     console.log(`1) [recommendationsController.js/getRecommendedMatches] Getting recommended matches for ${params.username}`);
     getRecMatches(params, (matches) => {
-      console.log(`4) [recommendationsController.js/getRecommendedMatches] Success! Building res object`);
+      console.log('4) [recommendationsController.js/getRecommendedMatches] Success! Building res object');
 
       const matchedUsers = matches.map((matchedUserData, index) => {
         console.log(`4-${index}) [recommendationsController.js/getRecommendedMatches] parsing data`);
           // Getting User data
-        const { properties: { memberSince, name, username } } = matchedUserData.get('recUsers');
+        const { properties: { memberSince, name, username, image_url, online } } = matchedUserData.get('recUsers');
         const city = matchedUserData.get('city').properties.name;
         const age = matchedUserData.get('age').properties.age;
         const sex = matchedUserData.get('sex').properties.sex;
-        const user = { memberSince, name, username, city, age, sex };
+        const user = { memberSince, name, username, image_url, online, city, age, sex };
 
         return user;
       });
-      console.log(`5) [recommendationsController.js/getRecommendedMatches] Sending potential matches!`, matchedUsers);
+      console.log('5) [recommendationsController.js/getRecommendedMatches] Sending potential matches!', matchedUsers);
       res.json(matchedUsers);
     });
   },
