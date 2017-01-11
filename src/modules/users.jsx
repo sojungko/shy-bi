b6c66d56d129e441ff57bb3daa434842e4c9d5da
JW Garrison <jwolfgarrison@gmail.com>
2017-01-10 19:17:04 -0500

    style(Online indicator):

--- src/modules/users.jsx
+++ src/modules/users.jsx
@@ -5,9 +5,8 @@ const renderUserList = (users, handleClick) => users
   .filter(({ username }) => username !== getUsername())
   .map(({ name, sex, age, city, image_url, username, online }, index) => (
     <li key={index} onClick={() => handleClick(username)}>
-      {online && <h5> ONLINE </h5>}
       <img role="presentation" src={image_url} />
-      <h3>{name}</h3>
+      <h3>{name}</h3>{online && 'ONLINE'}
       <h5>Sex: {sex}</h5>
       <h5>Age: {age}</h5>
       <h5>City: {city}</h5>
