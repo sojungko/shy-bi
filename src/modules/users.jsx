7869386c112a04670315e0b0601528976fdab56a
Sojung Park <sojungko@gmail.com>
2017-01-11 19:08:39 -0500

    feat(OnlineIcon): Render green online icon next to users, set background color for menu icon

--- src/modules/users.jsx
+++ src/modules/users.jsx
@@ -1,23 +1,25 @@
 import React from 'react';
 import { GridTile } from 'material-ui/GridList';
-import IconButton from 'material-ui/IconButton';
-import StarBorder from 'material-ui/svg-icons/toggle/star-border';
 import { getUsername } from '../modules/auth';
 
 const renderUserList = (users, handleClick) => users
   .filter(({ username }) => username !== getUsername())
-  .map(({ name, sex, age, city, image_url, username, online }, index) => (
-    <GridTile
-      key={index}
-      title={name}
-      style={{ opacity: '50%', fontFamily: 'PT Sans', cursor: 'pointer' }}
-      titleStyle={{ fontFamily: 'PT Sans', fontSize: '200%' }}
-      subtitle={<span style={{ fontSize: '17px' }}>{city}</span>}
-      actionIcon={<IconButton><StarBorder color="white" /></IconButton>}
-      onClick={() => handleClick(username)}
-    >
-      <img role="presentation" src={image_url} />
-    </GridTile>
-  ));
+  .map(({ name, sex, age, city, image_url, username, online }, index) => {
+    const renderOnlineIcon = bool => bool && <i className="fa fa-circle" style={{ color: '#81C784' }} aria-hidden="true" />;
+    return (
+      <GridTile
+        key={index}
+        title={name}
+        style={{ opacity: '50%', fontFamily: 'PT Sans', cursor: 'pointer' }}
+        titleStyle={{ fontFamily: 'PT Sans', fontSize: '200%' }}
+        subtitle={<span style={{ fontSize: '17px' }}>{city}</span>}
+        actionIcon={renderOnlineIcon(online)}
+        onClick={() => handleClick(username)}
+      >
+        <img role="presentation" src={image_url} />
+      </GridTile>
+    );
+  },
+);
 
 export default renderUserList;
