04fdf7d1b9469a6d4405f0036646f622f4517360
Sojung Park <sojungko@gmail.com>
2017-05-03 16:48:57 -0400

    Restyle about us page

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -3,21 +3,24 @@ import { Card, CardTitle, CardText } from 'material-ui/Card';
 
 const titleStyle = {
   fontSize: '400%',
-  fontFamily: 'Eskell',
+  fontFamily: 'Source Sasns Pro',
   textTransform: 'uppercase',
 };
 const nameTitleStyle = {
   fontSize: '200%',
-  fontFamily: 'Eskell',
+  fontFamily: 'Source Sans Pro',
+  marginLeft: '50px',
 };
 const textStyle = {
   group: {
     fontSize: '20px',
-    fontFamily: 'Maria',
+    fontFamily: 'Source Sans Pro',
+    marginLeft: '50px',
   },
   fontSize: '20px',
-  fontFamily: 'Maria',
+  fontFamily: 'Source Sans Pro',
   lineHeight: '25px',
+  marginLeft: '50px',
 };
 
 const AboutUs = () => (
@@ -67,11 +70,9 @@ const AboutUs = () => (
       </a>
       <CardText
         style={textStyle}
-      >
-        <p>Like JW, Sojung hails from South Korea and moved to New York by herself at 17.</p>
-        <p>After graduating from Northwestern University with a B.A. in chemistry, she worked as an English journalist in South Korea for five years.</p>
-        <p>She recently returned to New York to relaunch her career as a software engineer.</p>
-        <p>In her free time, she loves to play with make-up, post product reviews on YouTube and occasionally dabble in poker.</p>
+      >  
+        <p>Worked as a journalist for five years in Korea before deciding to switch to programming.</p>
+        <p>Regrets not having done that sooner.</p>
       </CardText>
     </Card>
     <Card>
@@ -84,7 +85,7 @@ const AboutUs = () => (
       <CardText
         style={textStyle}
       >
-        <p>I'm Peterr</p>
+        <p>A gracefully aging white 30-something man</p>
       </CardText>
     </Card>
   </div>
